7566658ebbd386e0011f0634da9f251ff8d0e076
Eduardo Lemos Júnior <elemosjr@gmail.com>
2020-08-25 22:11:44 -0300

    render cap 3

---
+++ apostila/_bookdown_files/rmarkdown_cache/html/unnamed-chunk-16_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.5/sourcecpp_28e3e05f16da24/file28e3e05d426314.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('/home/edu/Documents/estats/cursos/curso_rmarkdown/apostila/rmarkdown_cache/html/unnamed-chunk-16_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.5/sourcecpp_28e3e05f16da24/sourceCpp_2.so')
+
+timesTwo <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_timesTwo')
+
+rm(`.sourceCpp_1_DLLInfo`)
